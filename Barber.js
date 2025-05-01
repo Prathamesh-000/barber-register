aacd5e987de51cdd42c26e8bc85e15d21cf4c7d1
Prathamesh-000 <pprathamesh001@gmail.com>
2025-05-01 09:28:27 +0530

    Add files via upload
    added Barber.js

--- Barber.js
+++ Barber.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
 
 const BarberSchema = new mongoose.Schema({
-  name: { type: String, required: true, trim: true },
-  email: { type: String, required: true, unique: true, lowercase: true },
-  phone: { type: String, required: true ,unique:true},
-  password: { type: String, required: true },
+  name: { type: String, required: true },
   specialization: { type: String, required: true },
-  gender: { type: String, enum: ['Male', 'Female', 'Other'], required: true },
-  createdAt: { type: Date, default: Date.now }
+  email: { type: String, required: true, unique: true },
+  phone: { type: String, required: true },
+  gender: {
+    type: String,
+    enum: ['male', 'female', 'other'], // Allowed values
+    required: true
+  }
 });
 
 module.exports = mongoose.model('Barber', BarberSchema);
